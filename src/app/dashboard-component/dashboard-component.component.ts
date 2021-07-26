afc6c32cf4ec6c727284f0a73b450af9a4273536
Anna Ulanecka <ania.ulanecka@gmail.com>
2021-07-26 06:03:22 +0200

    update dashboard and favicon

--- src/app/dashboard-component/dashboard-component.component.ts
+++ src/app/dashboard-component/dashboard-component.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { TokenServiceService } from '../service/token-service.service';
+import { Friends } from '../model/friends';
 
 @Component({
   selector: 'app-dashboard-component',
@@ -9,7 +10,40 @@ import { TokenServiceService } from '../service/token-service.service';
 })
 export class DashboardComponentComponent implements OnInit {
   public login = window.sessionStorage.getItem('currentloggedin');
-  
+  friends: Friends[] = 
+  [
+    {
+        "id": 1,
+        "login": "awrobel",
+        "name": "Ania",
+        "lastname": "Wróbel" 
+    },
+    {
+        "id": 2,
+        "login": "bszyma",
+        "name": "Borys",
+        "lastname": "Szyma" 
+    },
+    {
+        "id": 3,
+        "login": "mzwierzynski",
+        "name": "Michał",
+        "lastname": "Zwierzyński" 
+    },
+    {
+        "id": 4,
+        "login": "jkapela",
+        "name": "Jaś",
+        "lastname": "Kapela" 
+    },
+    {
+        "id": 5,
+        "login": "kmojzesza",
+        "name": "Klapki",
+        "lastname": "Mojżesza" 
+    }
+];
+
   constructor(private router: Router, private TokenService: TokenServiceService) { }
 
   ngOnInit(): void {
---
+++ src/app/model/friends.ts
@@ -0,0 +1,7 @@
+export interface Friends
+{
+    id: number,
+    login: string,
+    name: string,
+    lastname: string
+}
