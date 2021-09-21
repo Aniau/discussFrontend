bb275fc689b2697984d3605844f54df2e52df217
AniaU <ania.ulanecka@gmail.com>
2021-09-21 12:30:42 +0200

    add messages model and sending

--- src/app/dashboard-component/dashboard-component.component.ts
+++ src/app/dashboard-component/dashboard-component.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { TokenServiceService } from '../service/token-service.service';
 import { Friends } from '../model/friends';
 import * as signalR from '@aspnet/signalr';
+import { Message } from '../model/message';
 
 @Component({
   selector: 'app-dashboard-component',
@@ -12,8 +13,10 @@ import * as signalR from '@aspnet/signalr';
 export class DashboardComponentComponent implements OnInit {
   public login = window.sessionStorage.getItem('currentloggedin');
   public message: string;
+  public currentFriendToTalk: string;
+  public messages: Message[] = [];
 
-  friends: Friends[] = 
+  public friends: Friends[] = 
   [
     {
         "id": 1,
@@ -51,21 +54,44 @@ export class DashboardComponentComponent implements OnInit {
 
   ngOnInit(): void {
     console.log(window);
-    const connection = new signalR.HubConnectionBuilder()
-    .configureLogging(signalR.LogLevel.Information)
-    .withUrl("https://localhost:44398/notify")
-    .build();
+  //   const connection = new signalR.HubConnectionBuilder()
+  //   .configureLogging(signalR.LogLevel.Information)
+  //   .withUrl("https://localhost:44398/notify")
+  //   .build();
 
-  connection.start().then(function () {
-    console.log('Połączenie nawiązane poprawnie');
-  }).catch(function (err) {
-    return console.error(err.toString());
-  });
-
-  connection.on("BroadcastMessage", (message: string) => {
-    this.message = message;
-  });
+  // connection.start().then(function () {
+  //   console.log('Połączenie nawiązane poprawnie');
+  // }).catch(function (err: string) {
+  //   return console.error(err.toString());
+  // });
   }
+  
+    talkToFriend(friend: any)
+    {
+      console.log(friend);
+      this.currentFriendToTalk = friend.name + ' ' + friend.lastname + ' (' + friend.login + ')';
+    }
+  
+    sendMesagge(message: string)
+    {
+      
+      console.log(message);
+      const messageData: Message = 
+      {
+        message: message,
+        send: true,
+        date: new Date(),
+        login: this.login,
+      } 
+
+      console.log(messageData);
+      console.log(this.messages);
+      this.messages.push(messageData);
+      this.message = '';
+        // connection.on("BroadcastMessage", (message: string) => {
+    //   this.mesaage = message;
+    // });
+    }
 
   logout(): void
   {
